df9726d46f5882a6eaa88a2ce554436958930fa4
John Pope <jp@fieldstormapp.com>
2017-05-14 14:55:36 -0400

    swift 3 updates - fix for https://github.com/haginile/SwiftAccelerate/issues/2

--- SwiftAccelerate.playground/section-1.swift
+++ SwiftAccelerate.playground/section-1.swift
@@ -1,12 +1,12 @@
 import Accelerate
 
 /**
-*  Vector & Scalar
-*/
+ *  Vector & Scalar
+ */
 
-var v = [1.0, 2.0]
+var v = [1.0, 0.0]
 var s = 3.0
-var vsresult = [Double](count : v.count, repeatedValue : 0.0)
+var vsresult = [Double](repeating : 0.0, count : v.count)
 vDSP_vsaddD(v, 1, &s, &vsresult, 1, vDSP_Length(v.count))
 vsresult
 
@@ -18,11 +18,11 @@ vsresult
 
 
 /**
-*  Vector & Vector
-*/
+ *  Vector & Vector
+ */
 var v1 = [2.0, 5.0]
 var v2 = [3.0, 4.0]
-var vvresult = [Double](count : 2, repeatedValue : 0.0)
+var vvresult = [Double](repeating : 0.0, count : 2)
 vDSP_vaddD(v1, 1, v2, 1, &vvresult, 1, vDSP_Length(v1.count))
 vvresult
 
@@ -34,8 +34,8 @@ vvresult
 
 
 /**
-*  Dot Product
-*/
+ *  Dot Product
+ */
 var v3 = [1.0, 2.0]
 var v4 = [3.0, 4.0]
 var dpresult = 0.0
@@ -44,49 +44,39 @@ dpresult
 
 
 /**
-*  Matrix Multiplication
-*/
+ *  Matrix Multiplication
+ */
 var m1 = [ 3.0, 2.0, 4.0, 5.0, 6.0, 7.0 ]
 var m2 = [ 10.0, 20.0, 30.0, 30.0, 40.0, 50.0]
-var mresult = [Double](count : 9, repeatedValue : 0.0)
+var mresult = [Double](repeating : 0.0, count : 9)
 
 vDSP_mmulD(m1, 1, m2, 1, &mresult, 1, 3, 3, 2)
 mresult
 
 
 /**
-*  Matrix Inversion
-*/
+ *  Matrix Inversion
+ */
 func invert(matrix : [Double]) -> [Double] {
-    
     var inMatrix = matrix
-    
     var N = __CLPK_integer(sqrt(Double(matrix.count)))
-    
-    var pivot : __CLPK_integer = 0
-    var workspace = 0.0
+    var pivots = [__CLPK_integer](repeating: 0, count: Int(N))
+    var workspace = [Double](repeating: 0.0, count: Int(N))
     var error : __CLPK_integer = 0
-    
-    
-    dgetrf_(&N, &N, &inMatrix, &N, &pivot, &error)
-    
-    if error != 0 {
-        return inMatrix
-    }
-    
-    dgetri_(&N, &inMatrix, &N, &pivot, &workspace, &N, &error)
+    dgetrf_(&N, &N, &inMatrix, &N, &pivots, &error)
+    dgetri_(&N, &inMatrix, &N, &pivots, &workspace, &N, &error)
     return inMatrix
 }
 
 var m = [1.0, 2.0, 3.0, 4.0]
-invert(m)
+invert(matrix: m)
 
 
 /**
-*  Matrix Transpose
-*/
+ *  Matrix Transpose
+ */
 
 var t = [1.0, 2.0, 3.0, 4.0, 5.0, 6.0]
-var mtresult = [Double](count : t.count, repeatedValue : 0.0)
+var mtresult = [Double](repeating : 0.0, count : t.count)
 vDSP_mtransD(t, 1, &mtresult, 1, 3, 2)
 mtresult
